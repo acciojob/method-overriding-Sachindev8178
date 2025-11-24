bcbcf0ae77c09b9a04cb9e20109a9bb7e0c60931
Sachindev8178 <sachinkumardev076@gmail.com>
2025-11-24 22:40:32 +0530

    Solved Opps Question Method overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        B b1=new B();
+        A b1=new B();
         System.out.println(b1.meth());
     }
   
